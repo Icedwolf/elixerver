c2a820f4c0e317ea3d9c5887f1350292998975d0
icedwolf <romaxino@gmail.com>
2019-09-11 08:32:53 -0300

    Sample Elixir Sever

---
+++ lib/http/application.ex
@@ -0,0 +1,13 @@
+defmodule Http.Application do
+  @moduledoc false
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {Http, port: 8080}
+    ]
+
+    opts = [strategy: :one_for_one, name: Http.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
